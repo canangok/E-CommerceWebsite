0ee475cd27d608b96ccb70a10d293ad11a9f4735
canangok <canangok186@gmail.com>
2019-12-29 16:53:37 +0300

    User entiti and LoginPage added

---
+++ E-CommerceWebSite.Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceWebSite.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Hakkımda bilgisini göstermek için bu alan zorunludur")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Hakkımda bilgisini göstermek için bu alan zorunludur")]
+        [StringLength(20, ErrorMessage = "Şifre en az {1}, en fazla {2} karakter olabilir", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        public int UserRole { get; set; }
+    }
+}
---
+++ E-CommerceWebSite.UI/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿
+using E_CommerceWebSite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_CommerceWebSite.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private static List<User> users;
+
+        public LoginController()
+        {
+            if (users == null)
+            {
+                users = new List<User>()
+                {
+                    new User{Id=1,Email="Ümit",Password="123"},
+                    new User{Id=2,Email="İrem",Password="123"},
+                    new User{Id=3,Email="Değişken",Password="123"},
+                    new User{Id=3,Email="Erkan",Password="123"},
+                };
+            }
+        }
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return PartialView("SignUp");
+
+        }
+
+
+        [HttpPost]
+        [NonAction]
+        public ActionResult AddUser(User User)
+        {
+            var user = new User
+            {
+                Id = users.Count + 1,
+                Email = User.Email,
+                Password = User.Password
+            };
+
+            users.Add(user);
+
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "İçeriği düzenleyebilmek için gerekli tüm alanları doldurmanız gerekmektedir");
+                return RedirectToAction("Index");
+            }
+
+            return Content("Kayıt Başarılı");
+            // return RedirectToAction("Index");
+            // return Json(users, JsonRequestBehavior.AllowGet);
+         }
+
+        public ActionResult GetUser(User User)
+        {
+            return Json(users, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
